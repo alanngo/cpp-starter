0069a903338148ee05190b3f1c72e12b7f4235e7
alan <alanngo673@gmail.com>
2024-06-05 10:40:41 -0500

    install googletest script

---
+++ scripts/install_gtest.sh
@@ -0,0 +1,17 @@
+# mark current workspace directory
+PROJ_DIR=$PWD
+
+# install and unpack gtest files
+sudo apt-get install cmake # install cmake
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+ 
+# copy or symlink libgtest.a and libgtest_main.a to your /usr/lib folder
+sudo cp *.a /usr/lib
+
+# navigate back to workspace
+cd $PROJ_DIR
+
+# Lets see if it works
+make test
